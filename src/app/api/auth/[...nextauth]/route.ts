352548aad9323b8d4d73b2716d746066c79abe27
rockomatthews <rob@robmatthe.ws>
2025-10-30 16:13:23 -0600

    4th commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
